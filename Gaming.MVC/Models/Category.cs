3d1ae4580f16e04283488e3f5c3cee15b9429da9
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-13 16:19:16 +0500

    added models and pages

---
+++ Gaming.MVC/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gaming.MVC.Models;
+
+public class Category
+{
+    [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int CategoryId { get; set; } 
+    public string CategoryName { get; set; }= null!; 
+    public string? CategoryImage { get; set; } 
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
