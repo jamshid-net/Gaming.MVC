d0f0581b2e1225d8bfa8a3012e1e8e901a670822
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-21 23:52:56 +0500

    tested bot

--- Gaming.MVC/Controllers/BotController.cs
+++ Gaming.MVC/Controllers/BotController.cs
@@ -46,5 +46,10 @@ public class BotController : Controller
        
     }
 
+    //[HttpPost("/api/bot")]
+    //public Task<IActionResult> TelegramUpdate(Update update)
+    //{
+        
+    //}
   
 }
