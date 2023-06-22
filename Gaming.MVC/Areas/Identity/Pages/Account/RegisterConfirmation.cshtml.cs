9dc55550ea0d29e2721db99ca5f0e5dc07d61849
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-22 13:01:22 +0500

    added google authen

--- Gaming.MVC/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ Gaming.MVC/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -62,7 +62,7 @@ namespace Gaming.MVC.Areas.Identity.Pages.Account
 
             Email = email;
             // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = false;
+            DisplayConfirmAccountLink = true;
             if (DisplayConfirmAccountLink)
             {
                 var userId = await _userManager.GetUserIdAsync(user);
